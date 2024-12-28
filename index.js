adcf979ba636ed63337820b473154823003ade6a
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-12-28 10:52:42 +0530

    aws

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get("/", (req, res) => {
   res.send({ status: "success", message: "node js server started" });
 });
 
+app.get("/test", (req, res) => {
+  res.send({ status: "success", message: "aws node server" });
+});
 app.listen(8090, () => {
   console.log("listening on port 8090");
 });
